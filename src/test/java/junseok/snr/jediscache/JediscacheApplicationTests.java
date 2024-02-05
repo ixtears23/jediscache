f9fb8753302dcd1f752039b13c2b7e080d377310
JunseokOh <ixtears23@icloud.com>
2024-02-05 19:52:59 +0900

    프로젝트생성

---
+++ src/test/java/junseok/snr/jediscache/JediscacheApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.jediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediscacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
