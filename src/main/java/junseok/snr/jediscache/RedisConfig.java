acf9cb651b1fc97dade8b87a1ecd329b03ce6001
JunseokOh <ixtears23@icloud.com>
2024-02-16 19:42:56 +0900

    JedisPool UnableToRegisterMBeanException 문제 해결

--- src/main/java/junseok/snr/jediscache/RedisConfig.java
+++ src/main/java/junseok/snr/jediscache/RedisConfig.java
@@ -1,14 +1,17 @@
 package junseok.snr.jediscache;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
-@Component
+@Configuration
 public class RedisConfig {
 
     @Bean
     public JedisPool createJedisPool() {
-        return new JedisPool("127.0.0.1", 6379);
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setJmxEnabled(false); // JMX 등록 비활성화
+        return new JedisPool(poolConfig, "127.0.0.1", 6379);
     }
 }
