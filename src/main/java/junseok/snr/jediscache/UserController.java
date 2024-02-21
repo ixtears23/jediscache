2bf8738a78d5e96e88df376a707314de34908d04
JunseokOh <ixtears23@icloud.com>
2024-02-21 22:06:44 +0900

    Cache-Aside pattern 적용코드 추가

--- src/main/java/junseok/snr/jediscache/UserController.java
+++ src/main/java/junseok/snr/jediscache/UserController.java
@@ -4,14 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
     private final UserRepository userRepository;
+    private final JedisPool jedisPool;
 
     @GetMapping("/users/{id}/email")
     public String getUserEmail(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(User.builder().build()).getEmail();
+        try (Jedis jedis = jedisPool.getResource()) {
+            var userEmailRedisKey = "users:%d:email".formatted(id);
+
+            String userEmail = jedis.get(userEmailRedisKey);
+            if (userEmail != null) {
+                return userEmail;
+            }
+
+            userEmail = userRepository.findById(id).orElse(User.builder().build()).getEmail();
+            jedis.set(userEmailRedisKey, userEmail);
+            jedis.setex(userEmailRedisKey, 30, userEmail);
+
+            return userEmail;
+        }
+
     }
 }
